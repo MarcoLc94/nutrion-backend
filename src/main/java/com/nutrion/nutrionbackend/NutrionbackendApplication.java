11e1ba9df9e50c970068f86bffafa1353282bf05
marco <marcoantoniolopezcastro>
2026-01-06 16:56:59 -0600

    first commit

---
+++ src/main/java/com/nutrion/nutrionbackend/NutrionbackendApplication.java
@@ -0,0 +1,13 @@
+package com.nutrion.nutrionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutrionbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutrionbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nutrion/nutrionbackend/NutrionbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutrion.nutrionbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutrionbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
